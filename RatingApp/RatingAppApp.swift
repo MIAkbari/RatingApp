f54d31616e08892572866f709b80f50c552468f1
Mohammad <makbariengineer@gmail.com>
2025-09-06 15:34:16 +0330

    Initial Commit

---
+++ RatingApp/RatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingAppApp.swift
+//  RatingApp
+//
+//  Created by Mohammad on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
